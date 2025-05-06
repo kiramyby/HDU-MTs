8d8d66a877d733bf307716db0c426845a5c519e0
Kiracoon <kiramyby@gmail.com>
2025-05-06 11:32:46 +0800

    fix(csapp_program): multi_alu (rough)

--- csapp_program/11-3_alu/multi_alu.v
+++ csapp_program/11-3_alu/multi_alu.v
@@ -1,18 +1,21 @@
 module multi_alu (
-    input  wire        clk,
-    input  wire        clk_A,
-    input  wire        clk_B,
-    input  wire        clk_F,
-    input  wire        rst_n,    
-    input  wire [31:0] sw,
-
-    // Flag, LED display
-    output reg  [3:0]  FR,
-
-    // Result, seg digit display
-    output wire [7:0] SEG,
-    output wire [2:0] WHICH
+    clk, clk_A, clk_B, clk_F, rst_n,
+    sw, FR, SEG, WHICH
 );
+
+input  wire        clk;
+input  wire        clk_A;
+input  wire        clk_B;
+input  wire        clk_F;
+input  wire        rst_n;    
+input  wire [31:0] sw;
+
+// FLAG LED显示
+output wire [3:0]  FR;
+
+// Result 数码管显示
+output wire [7:0] SEG;
+output wire [2:0] WHICH;
 
 wire [31:0] alu_a;
 wire [31:0] alu_b;
@@ -20,12 +23,9 @@ wire [31:0] alu_f;
 wire [3:0]  alu_op = sw[31:28];
 wire [3:0]  alu_fr;
 
-reg  [31:0] F;
+wire [31:0] F;
 
 alu ALU (
-    .clk_A(clk_A),
-    .clk_B(clk_B),
-    .clk_F(clk_F),
     .ALU_OP(alu_op),
     .ALU_A(alu_a),
     .ALU_B(alu_b),
@@ -33,7 +33,7 @@ alu ALU (
     .SF(alu_fr[2]),
     .OF(alu_fr[1]),
     .CF(alu_fr[0]),
-    .ALU_F(alu_f),
+    .ALU_F(alu_f)
 );
 
 reg_b32 A (
@@ -48,14 +48,14 @@ reg_b32 B (
     .d(sw),
     .q(alu_b)
 );
-reg_b32 F (
+reg_b32 result_reg (
     .clk(clk_F),
     .rst_n(rst_n),
     .d(alu_f),
     .q(F)
 );
 
-reg_b4 FR (
+reg_b4 flag_reg (
     .clk(clk_F),
     .rst_n(rst_n),
     .d(alu_fr),
@@ -64,7 +64,7 @@ reg_b4 FR (
 
 display display_inst (
     .clk(clk),
-    .data(ALU_F),
+    .data(F),
     .which(WHICH),
     .seg(SEG)
 );
