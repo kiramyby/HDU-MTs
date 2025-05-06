ef80c963ed0fcab8e8d5340e11fac5caeeaba8d3
Kiracoon <kiramyby@gmail.com>
2025-05-06 11:14:47 +0800

    fix(csapp_program): alu final

--- csapp_program/11-3_alu/alu.v
+++ csapp_program/11-3_alu/alu.v
@@ -1,37 +1,40 @@
 module alu (
-    input  wire [3:0]  ALU_OP,      
-    input  wire [31:0] ALU_A,       
-    input  wire [31:0] ALU_B,       
-    output reg  [31:0] ALU_F,
-    output wire        ZF,           // Zero Flag
-    output wire        CF,           // Carry Flag
-    output wire        OF,           // Overflow Flag
-    output wire        SF            // Sign Flag
+    ALU_OP, ALU_A, ALU_B, ALU_F,
+    ZF, CF, OF, SF
 );
+
+input  wire [3:0]  ALU_OP;      
+input  wire [31:0] ALU_A;      
+input  wire [31:0] ALU_B;       
+output reg  [31:0] ALU_F;
+output wire        ZF;           // 零标志
+output wire        CF;           // 进位标志
+output wire        OF;           // 溢出标志
+output wire        SF;           // 符号标志
 
 reg C32 = 1'b0;
 
 always @(ALU_OP, ALU_A, ALU_B)
 begin
     case (ALU_OP)
-        4'b0000: begin  // add
+        4'b0000: begin  // 加法 add
             {C32, ALU_F} = ALU_A + ALU_B;
         end
-        4'b0001: begin  // sll
+        4'b0001: begin  // 左移 sll
             {C32, ALU_F} = {1'b0, ALU_A << ALU_B[4:0]};
         end
-        4'b0010: ALU_F = ($signed(ALU_A) < $signed(ALU_B)) ? 32'd1 : 32'd0;  // slt
-        4'b0011: ALU_F = (ALU_A < ALU_B) ? 32'd1 : 32'd0;  // sltu
-        4'b0100: ALU_F = ALU_A ^ ALU_B;           // xor
-        4'b0101: begin  // srl
+        4'b0010: ALU_F = ($signed(ALU_A) < $signed(ALU_B)) ? 32'd1 : 32'd0;  // 有符号数比较小于置数 slt
+        4'b0011: ALU_F = (ALU_A < ALU_B) ? 32'd1 : 32'd0;  // 无符号数比较小于置数 sltu
+        4'b0100: ALU_F = ALU_A ^ ALU_B;           // 异或 xor
+        4'b0101: begin  // 逻辑右移 srl
             {C32, ALU_F} = {1'b0, ALU_A >> ALU_B[4:0]};
         end
-        4'b0110: ALU_F = ALU_A | ALU_B;           // or
-        4'b0111: ALU_F = ALU_A & ALU_B;           // and
-        4'b1000: begin  // sub
+        4'b0110: ALU_F = ALU_A | ALU_B;           // 按位或 or
+        4'b0111: ALU_F = ALU_A & ALU_B;           // 按位与 and
+        4'b1000: begin  // 减法 sub
             {C32, ALU_F} = ALU_A - ALU_B;
         end
-        4'b1101: begin  // sra
+        4'b1101: begin  // 算术右移 sra
             {C32, ALU_F} = {ALU_A[31], $signed(ALU_A) >>> ALU_B[4:0]};
         end
         default: {C32, ALU_F} = 33'b0;
@@ -41,8 +44,8 @@ end
 // FlagU
 assign ZF = (ALU_F == 32'd0);
 assign SF = ALU_F[31];
-assign CF = ((ALU_OP == 4'b0000) || (ALU_OP == 4'b1000) || 
-            (ALU_OP == 4'b0001) || (ALU_OP == 4'b0101) || (ALU_OP == 4'b1101)) ? C32 : !C32;
+assign CF = (ALU_OP == 4'b0000) ? C32 :
+            (ALU_OP == 4'b1000) ? ~C32 : 1'b0;
 assign OF = ALU_A[31] ^ ALU_B[31] ^ C32 ^ ALU_F[31];
 
 endmodule
