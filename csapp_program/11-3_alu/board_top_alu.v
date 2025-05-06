a9a1992149cbce82c382951979d2e72fd7b21dd0
kiracoon <kiramyby@gmail.com>
2025-05-06 11:22:48 +0800

    feat(csapp_program): board_top_alu (rough)

---
+++ csapp_program/11-3_alu/board_top_alu.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+module board_top(
+    input   wire          clk,
+    input   wire [4:1]    swb,
+    input   wire [31:0]   sw,
+    output  wire [3:0]    led,
+    output  wire [7:0]    seg,
+    output  wire [2:0]    which
+);
+
+multi_alu alu_lab (
+    .clk(clk),
+    .rst_n(swb[4]),
+    .clk_A(swb[1]),
+    .clk_B(swb[2]),
+    .clk_F(swb[3]),
+    .sw(sw),
+    .FR(led),
+    .seg(seg),
+    .which(which)
+);
+
+endmodule
