b9084657a8849378d6b4fbf6ddf46e7bc4751444
Kiracoon <kiramyby@gmail.com>
2025-06-03 06:18:59 +0800

    fix(csapp_program): prepare for final cpu

---
+++ csapp_program/final_cpu/run.s
@@ -0,0 +1,19 @@
+main:
+    addi    a0      zero,   0x10
+    ori     a1,     zero,   3
+    xori    a2,     zero,   0x30
+    jal     BankSum
+    lw      s0,     0(a2)
+    add     zero,   zero,   zero
+    add     zero,   zero,   zero
+BankSum:
+    add     t0,     a0,     zero
+    or      t1,     a1,     zero
+    and     t2,     zero,   zero
+L:  lw      t3,     0(t0)
+    add     t2,     t2,     t3
+    addi    t0,     t0,     4
+    beq     t1,     zero,exit
+    jal     L
+exit: sw    t2,0(a2)
+      jr    ra
